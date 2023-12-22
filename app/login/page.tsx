26277aba5d9b550ccb85432dccba86aef2907818
awu <softengineerwu@gmail.com>
2023-12-22 22:20:23 +0800

    增加登录表单的状态控制

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import {
   EyeInvisibleOutlined,
   EyeTwoTone,
@@ -16,6 +16,8 @@ import {
   Col,
   Layout,
   Flex,
+  Alert,
+  Spin
 } from "antd";
 
 const { Header, Footer, Sider, Content } = Layout;
@@ -24,11 +26,19 @@ import { useRouter } from "@/node_modules/next/navigation";
 
 import "./style.css";
 
-
 export default function Login() {
   const router = useRouter();
 
+  //是否等待加载中
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [loginFailMsg, setLoginFailMsg] = useState("");
+  const [loginFail, setLoginFail] = useState(false);
+
+  //表单提交执行逻辑
   const onFinish = async (values) => {
+    setIsLoading(true);
+
     const { username, password } = values;
     const reqData = { username, password };
     try {
@@ -42,13 +52,26 @@ export default function Login() {
       if (response.ok) {
         const data = await response.json();
         console.log("success:", data);
-        router.push('/');
+        router.push("/");
       } else {
+        setLoginFail(true);
         const data = await response.json();
-        console.log("fail:", data);
+        //TODO:前端有一个错误码和国际化文字的对应关系，用错误码对应的文字显示
+        setLoginFailMsg(data.message);
       }
     } catch (error) {
-      console.error("error:", error);
+      setLoginFail(true);
+      setLoginFailMsg("登录发生异常，请重试");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  //登录按钮绑定回车
+  const handleSubmit = (event) => {
+    if (event.key === "Enter") {
+      // 表单提交
+      event.target.form.submit();
     }
   };
 
@@ -58,35 +81,48 @@ export default function Login() {
       <Content className="contentStyle">
         <div className="formSytle">
           <Divider>登录</Divider>
-          <Form name="login" className="login-form" onFinish={onFinish}>
-            <Form.Item
-              name="username"
-              rules={[{ required: true, message: "请输入用户名" }]}
-            >
-              <Input
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="用户名"
-              />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[{ required: true, message: "请输入密码" }]}
-            >
-              <Input.Password
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                placeholder="密码"
-              />
-            </Form.Item>
-            <Form.Item className="loginBtnSytle">
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="login-form-button"
+          {loginFail && (
+            <Alert
+              message={loginFailMsg}
+              type="error"
+              className="alert-style"
+            />
+          )}
+          <Spin spinning={isLoading}>
+            <Form name="login" className="login-form" onFinish={onFinish}>
+              <Form.Item
+                name="username"
+                rules={[{ required: true, message: "请输入用户名" }]}
               >
-                登录
-              </Button>
-            </Form.Item>
-          </Form>
+                <Input
+                  prefix={<UserOutlined className="site-form-item-icon" />}
+                  placeholder="用户名"
+                 
+                />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[{ required: true, message: "请输入密码" }]}
+              >
+                <Input.Password
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  placeholder="密码"
+             
+                />
+              </Form.Item>
+              <Form.Item className="loginBtnSytle">
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                  onKeyDown={handleSubmit}
+                  
+                >
+                  登录
+                </Button>
+              </Form.Item>
+            </Form>
+          </Spin>
         </div>
       </Content>
       <Footer className="footerStyle">©2023 Mortnon.</Footer>

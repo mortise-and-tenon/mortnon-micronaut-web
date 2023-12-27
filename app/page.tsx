63dd1ed9091a50a225b7090bf1832626f37a25d3
dev2007 <softengineerwu@gmail.com>
2023-12-27 16:26:10 +0800

    重新初始化next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,95 @@
-"use client";
-import React from "react";
-import Image from "next/image";
-import { Layout, Menu, theme,MenuProps,Avatar  } from "antd";
-import { HomeOutlined, SettingOutlined,UserOutlined  } from '@ant-design/icons';
-import Link from "next/link";
-const { Header, Content } = Layout;
-
-const menuItems:MenuProps = [
-  {
-    key: 'dashboard',
-    label: (<Link href="/">首页</Link>),
-    icon: <HomeOutlined/>
-  },
-  {
-    key: 'sys',
-    label: (<Link href="/user">系统管理</Link>),
-    icon: <SettingOutlined/>
-  }
-]
+import Image from 'next/image'
+import styles from './page.module.css'
 
 export default function Home() {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
   return (
-    <Layout className="layout-full-screen">
-      <Header
-        className="main-header"
-      >
-        <Image src="/clover.png" alt="Logo" width={48} height={48} />
-        <Menu
-          mode="horizontal"
-          defaultSelectedKeys={["dashboard"]}
-          items={menuItems}
-          style={{ flex: 1, minWidth: 0 }}
-          theme="light"
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>app/page.tsx</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
         />
-        <Avatar icon={<UserOutlined />} />
-      </Header>
-      <Content>Dashbord</Content>
-    </Layout>
-  );
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore starter templates for Next.js.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
 }
--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-/**
- * 增加开发用配置，当访问/api/xx时，映射到后端localhost:8080/xxx
- */
-module.exports = {
-  //代理重定向到后台服务
-  async rewrites() {
-    return {
-      fallback: [{
-        source: '/api/:path*',
-        destination: `http://localhost:8080/:path*`,
-      }, ],
-    }
-  },
-}
+module.exports = nextConfig

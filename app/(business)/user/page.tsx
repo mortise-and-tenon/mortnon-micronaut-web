5d2bfd0fb90fe1ca6f1f8f03888a36e739702741
dev2007 <softengineerwu@gmail.com>
2024-01-03 20:39:12 +0800

    用户管理，添加用户模态框输入及逻辑

--- app/(business)/user/page.tsx
+++ app/(business)/user/page.tsx
@@ -1,5 +1,14 @@
 "use client";
-import { Layout, Table, Breadcrumb, Card, Button,Modal } from "@douyinfe/semi-ui";
+import {
+  Layout,
+  Table,
+  Breadcrumb,
+  Card,
+  Button,
+  Modal,
+  Form,
+  Toast,
+} from "@douyinfe/semi-ui";
 
 import { IconUserAdd, IconRefresh } from "@douyinfe/semi-icons";
 
@@ -8,7 +17,7 @@ import NavSider from "@/app/_modules/navSider";
 
 import "../style.css";
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const { Content } = Layout;
 
@@ -26,7 +35,7 @@ enum Sex {
   Male = 1,
 }
 
-//查询数据定义
+//分页查询数据定义
 export type QueryInfo = {
   //查询页码
   page: number;
@@ -34,7 +43,7 @@ export type QueryInfo = {
   size: number;
 };
 
-//查询数据结果定义
+//查询用户数据结果定义
 export type QueryResult = {
   //当前页数
   pageNumber: number;
@@ -50,7 +59,7 @@ export type QueryResult = {
   userNameFilter: Array<ColumnFilter>;
 };
 
-//获取的用户信息定义
+//用户信息定义
 export type UserInfo = {
   //用户id对应表格key
   key: number;
@@ -65,8 +74,44 @@ export type UserInfo = {
   roleName: string;
 };
 
+//组织树信息定义
+export type ProjectTreeNode = {
+  key: string;
+  label: string;
+  children: Array<ProjectTreeNode>;
+};
+
+//角色信息定义
+export type RoleInfo = {
+  value: number;
+  label: string;
+};
+
+//创建用户的组织角色信息定义
+export type CreateUserProjectRole = {
+  projectId: number;
+  roleId: number;
+};
+
+//创建用户的信息定义
+export type CreateUserData = {
+  userName: string;
+  nickName: string;
+  password: string;
+  repeatPassword: string;
+  email: string;
+  phone: string;
+  head: string;
+  sex: number;
+  projectRoles: Array<CreateUserProjectRole>;
+};
+
 //获取用户数据
-export async function getUser(queryInfo, setQueryResult, setLoading) {
+export async function getUser(
+  queryInfo: QueryInfo,
+  setQueryResult: React.Dispatch<React.SetStateAction<QueryResult>>,
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+) {
   try {
     const queryParams = new URLSearchParams({
       page: queryInfo.page,
@@ -105,7 +150,7 @@ export async function getUser(queryInfo, setQueryResult, setLoading) {
         };
         userNameFilters.push(userNameFilter);
       });
-      console.log(userList);
+
       //绑定查询到的数据
       //前台semi默认页数从1开始，后端从0开始
       const queryResult: QueryResult = {
@@ -122,6 +167,79 @@ export async function getUser(queryInfo, setQueryResult, setLoading) {
   } catch (error) {
     console.log("error:", error);
     setLoading(false);
+  }
+}
+
+//获取树型组织信息
+export async function getProjectTree(
+  setProjectTree: React.Dispatch<React.SetStateAction<Array<ProjectTreeNode>>>
+) {
+  try {
+    const response = await fetch("/api/projects/tree");
+    if (response.ok) {
+      const body = await response.json();
+      const data = body.data;
+      const rootNode: ProjectTreeNode = {
+        key: data.id.toString(),
+        label: data.name,
+        children: [],
+      };
+      convertProjectNode(rootNode, data.children);
+
+      const topProjectTreeNodeArray: Array<ProjectTreeNode> =
+        new Array<ProjectTreeNode>();
+      topProjectTreeNodeArray.push(rootNode);
+      setProjectTree(topProjectTreeNodeArray);
+    }
+  } catch (error) {
+    console.log("query project error", error);
+  }
+}
+
+const convertProjectNode = (parentNode: ProjectTreeNode, dataList: []) => {
+  const children: Array<ProjectTreeNode> = new Array<ProjectTreeNode>();
+  dataList.forEach((data) => {
+    const node: ProjectTreeNode = {
+      key: data.id,
+      label: data.name,
+      children: [],
+    };
+
+    if (null != data.children) {
+      convertProjectNode(node, data.children);
+    }
+    children.push(node);
+  });
+
+  parentNode.children = children;
+  return parentNode;
+};
+
+//获取角色信息
+export async function getRole(
+  setRole: React.Dispatch<React.SetStateAction<Array<RoleInfo>>>,
+  setRoleLoading: React.Dispatch<React.SetStateAction<boolean>>
+) {
+  try {
+    const response = await fetch("/api/roles");
+    if (response.ok) {
+      const body = await response.json();
+      const data = body.data;
+
+      const roleArray: Array<RoleInfo> = new Array<RoleInfo>();
+      data.content.forEach((role) => {
+        const roleInfo: RoleInfo = {
+          value: role.id,
+          label: role.name,
+        };
+        roleArray.push(roleInfo);
+      });
+      setRole(roleArray);
+    }
+  } catch (error) {
+    console.log("get role fail:", error);
+  } finally {
+    setRoleLoading(false);
   }
 }
 
@@ -185,19 +303,42 @@ export default function User() {
     },
   ];
 
+  //表格加载状态
   const [loading, setLoading] = useState(true);
+  //表格默认分页数
   const defaultPageSize = 10;
+  //表格查询结果数据
   const [queryInfo, setQueryInfo] = useState({
     page: 0,
     size: defaultPageSize,
   } as QueryInfo);
+
+  //性别选项
+  const sexInfo = [
+    {
+      value: 1,
+      label: "男",
+    },
+    {
+      value: 0,
+      label: "女",
+    },
+  ];
+
+  //组织树数据
+  const [projectTree, setProjectTree] = useState([] as Array<ProjectTreeNode>);
+
+  //角色数据
+  const [roleInfo, setRoleInfo] = useState([] as Array<RoleInfo>);
+  //角色加载状态
+  const [roleLoading, setRoleLoading] = useState(true);
 
   useEffect(() => {
     getUser(queryInfo, setQueryResult, setLoading);
   }, [queryInfo]);
 
   //按条件刷新表格
-  const refreshTable = (currentPage, pageSize) => {
+  const refreshTable = (currentPage: number, pageSize: number) => {
     setLoading(true);
 
     setQueryInfo({
@@ -216,20 +357,104 @@ export default function User() {
     refreshTable(0, defaultPageSize);
   };
 
+  const formApiRef = useRef();
+
+  //获取Form的 formApi，用于模态框的按钮手动提交表单
+  const bindFormApi = (formApi) => {
+    formApiRef.current = formApi;
+  };
+
+  //是否展示模态框
   const [visible, setVisible] = useState(false);
   const showDialog = () => {
-      setVisible(true);
+    getProjectTree(setProjectTree);
+    getRole(setRoleInfo, setRoleLoading);
+    setVisible(true);
   };
+
+  //添加用户
+  const onSubmit = async (values) => {
+    console.log("create user:", values);
+    const createUserProjectRoleArray: Array<CreateUserProjectRole> =
+      new Array<CreateUserProjectRole>();
+
+    const userProjectRole: CreateUserProjectRole = {
+      projectId: values.projectid,
+      roleId: values.roleid,
+    };
+    createUserProjectRoleArray.push(userProjectRole);
+
+    const createUserData: CreateUserData = {
+      nickName: values.nickname,
+      userName: values.username,
+      password: values.password,
+      repeatPassword: values.repeatpassword,
+      email: values.email,
+      phone: values.phone,
+      head: values.head,
+      sex: values.sex,
+      projectRoles: createUserProjectRoleArray,
+    };
+
+    console.log("user:", createUserData);
+
+    try {
+      const response = await fetch("/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(createUserData),
+        credentials: "include",
+      });
+
+      //创建用户成功
+      if (response.ok) {
+        Toast.success("添加用户成功");
+        //操作成功后关闭模态框
+        setVisible(false);
+      } else {
+        Toast.error("添加用户失败");
+      }
+    } catch (error) {
+      console.log("create use fail:", error);
+      Toast.error("添加用户失败，请重试");
+    } finally {
+    }
+  };
+
+  //点击添加用户窗口确认按钮
   const handleOk = () => {
-      setVisible(false);
-      console.log('Ok button clicked');
+    if (formApiRef.current != null) {
+      formApiRef.current.submitForm();
+      console.log("Ok button clicked");
+    } else {
+      Toast.error("发生异常，请刷新重试");
+      console.log("formapi error,can't submit form.");
+    }
   };
   const handleCancel = () => {
-      setVisible(false);
-      console.log('Cancel button clicked');
+    setVisible(false);
+    console.log("Cancel button clicked");
   };
   const handleAfterClose = () => {
-      console.log('After Close callback executed');
+    console.log("After Close callback executed");
+  };
+
+  //校验重复密码要一致
+  const validateRepeatPwd = (val, values) => {
+    if (!val) {
+      return "请再次输入密码";
+    }
+    if (formApiRef.current != null) {
+      const pwd = formApiRef.current.getValue("password");
+      if (pwd != val) {
+        return "两次输入密码不一致";
+      }
+    } else {
+      Toast.error("发生异常，请刷新重试");
+      console.log("formapi error,can't validate pwd.");
+    }
   };
 
   return (
@@ -247,21 +472,95 @@ export default function User() {
               <Button
                 theme="borderless"
                 icon={<IconUserAdd />}
-                aria-label="新建用户"
+                aria-label="添加用户"
                 onClick={showDialog}
               />
               <Modal
-                title="基本对话框"
+                title="添加用户"
                 visible={visible}
+                centered
                 onOk={handleOk}
-                afterClose={handleAfterClose} //>=1.16.0
+                afterClose={handleAfterClose}
                 onCancel={handleCancel}
                 closeOnEsc={true}
-            >
-                This is the content of a basic modal.
-                <br />
-                More content...
-            </Modal>
+                maskClosable={false}
+              >
+                <Form
+                  wrapperCol={{ span: 18 }}
+                  labelCol={{ span: 6 }}
+                  labelPosition="left"
+                  labelAlign="right"
+                  onSubmit={onSubmit}
+                  getFormApi={bindFormApi}
+                >
+                  <Form.Input
+                    field="username"
+                    label="用户名"
+                    placeholder={"用户名"}
+                    trigger="blur"
+                    rules={[{ required: true, message: "请输入用户名" }]}
+                  />
+                  <Form.Input
+                    field="nickname"
+                    label="姓名"
+                    placeholder={"姓名"}
+                    trigger="blur"
+                    rules={[{ required: true, message: "请输入姓名" }]}
+                  />
+                  <Form.Select
+                    field="sex"
+                    label="性别"
+                    placeholder="请选择性别"
+                    style={{ width: "100%" }}
+                    optionList={sexInfo}
+                    rules={[{ required: true, message: "请选择性别" }]}
+                  />
+                  <Form.Input
+                    field="password"
+                    label="密码"
+                    mode="password"
+                    placeholder={"请输入密码"}
+                    trigger="blur"
+                    rules={[{ required: true, message: "请输入密码" }]}
+                  />
+                  <Form.Input
+                    field="repeatpassword"
+                    label={{ text: "确认密码", required: true }}
+                    mode="password"
+                    placeholder={"请再次输入密码"}
+                    trigger="blur"
+                    validate={validateRepeatPwd}
+                  />
+                  <Form.TreeSelect
+                    field="projectid"
+                    label="选择组织"
+                    placeholder="请选择组织"
+                    style={{ width: "100%" }}
+                    dropdownStyle={{ maxHeight: 400, overflow: "auto" }}
+                    treeData={projectTree}
+                    rules={[{ required: true, message: "请选择组织" }]}
+                  />
+                  <Form.Select
+                    field="roleid"
+                    label="选择角色"
+                    placeholder="请选择角色"
+                    style={{ width: "100%" }}
+                    loading={roleLoading}
+                    optionList={roleInfo}
+                    rules={[{ required: true, message: "请选择角色" }]}
+                  />
+                  <Form.Input
+                    field="email"
+                    label="电子邮箱"
+                    placeholder={"请输入电子邮箱"}
+                  />
+                  <Form.Input
+                    field="phone"
+                    label="手机号"
+                    placeholder={"请输入手机号"}
+                  />
+                </Form>
+              </Modal>
               <div>
                 <Button
                   theme="borderless"
